fc4a2cfc2a2f6a736af9e8cc009ec363baee5123
SatendraNegi <SatendraNegi@9.199.52.126>
2020-03-06 15:46:50 +0530

    ok

--- RestAPIforOpenShift/src/main/java/com/satendranegi/openshift/HelloWorld.java
+++ RestAPIforOpenShift/src/main/java/com/satendranegi/openshift/HelloWorld.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 public class HelloWorld {
 
-	@GetMapping(value = "/hello")
+	@RequestMapping("hello")
 	public ResponseEntity<String> helloworld(){
 		
 		return new ResponseEntity<String>("hello world from openshift",HttpStatus.OK);
