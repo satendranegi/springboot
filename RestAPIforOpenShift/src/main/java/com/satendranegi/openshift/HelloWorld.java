71b88b30759973e57e1954fe7b15d34e8b884143
SatendraNegi <SatendraNegi@DESKTOP-GH99R6G>
2020-03-07 00:42:04 +0530

    fix 1

--- RestAPIforOpenShift/src/main/java/com/satendranegi/openshift/HelloWorld.java
+++ RestAPIforOpenShift/src/main/java/com/satendranegi/openshift/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
 	@RequestMapping("hello")
 	public ResponseEntity<String> helloworld(){
 		
-		return new ResponseEntity<String>("hello world from openshift",HttpStatus.OK);
+		return new ResponseEntity<String>("hello world from openshift :) ",HttpStatus.OK);
 	}
 	
 	/*
