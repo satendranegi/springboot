6fdd7db7b2bce15c5e455cd4f7611473c91082d4
SatendraNegi <SatendraNegi@9.199.53.168>
2020-03-11 21:25:16 +0530

    Adding method to return booking by id

--- RestAPIforOpenShift/src/main/java/com/satendranegi/openshift/HelloWorld.java
+++ RestAPIforOpenShift/src/main/java/com/satendranegi/openshift/HelloWorld.java
@@ -1,10 +1,13 @@
 package com.satendranegi.openshift;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,7 +55,12 @@ public class HelloWorld {
 		return "Success";
 	}
 	
-	
+	@GetMapping("bookings/{id}")
+	public @ResponseBody Optional<BookingRequest> getBookingById(@PathVariable("id") long id){
+		
+		return bookingrequest.findById(id);
+		
+	}
 	
 	
 	
