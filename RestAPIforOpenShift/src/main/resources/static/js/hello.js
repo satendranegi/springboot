09ea69fd00278b2f35b4e8b07b64cd6403bb8cf5
SatendraNegi <SatendraNegi@host.docker.internal>
2020-04-05 12:44:17 +0530

    scope fix

--- RestAPIforOpenShift/src/main/resources/static/js/hello.js
+++ RestAPIforOpenShift/src/main/resources/static/js/hello.js
@@ -8,14 +8,7 @@ app.controller('Hello', function($scope, $http) {
 
 app.controller('Contact', function ($scope, $rootScope, $http) {
 	console.log("inside contact for js");
-    $scope.formData = {	
-    		
-    		"fullName":$scope.fullName,
-    		"emailId":$scope.emailId,
-    		"mobileNumber":$scope.mobileNumber,
-    		"message":$scope.message
-    };
-    console.log("form data..."+$scope.formData)
+    $scope.formData = {};
     $scope.contact = function () {
         $http.post('/contact', $scope.formData).then(
             function (resposne) {
