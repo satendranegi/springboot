eba503019fd1c1528ef17919b1045027247e62af
SatendraNegi <SatendraNegi@host.docker.internal>
2020-04-05 10:43:03 +0530

    changes

--- RestAPIforOpenShift/src/main/resources/static/js/hello.js
+++ RestAPIforOpenShift/src/main/resources/static/js/hello.js
@@ -5,3 +5,17 @@ angular.module('demo', [])
             $scope.greeting = response.data;
         });
 });
+.controller('contact', function ($scope, $rootScope, $http) {
+    $scope.formData = {};
+    $scope.contact = function () {
+        $http.post('/contact', $scope.formData).then(
+            function (resposne) {
+                console.log("registered");
+                console.log(response);
+            },
+            function (response) {
+                console.log("Register error")
+            }
+        )
+      }
+});
